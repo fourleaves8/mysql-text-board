6ee436b938a563e29ba4ce92cec4b8557edede63
fourleaves8 <fourleaves8@gmail.com>
2020-12-07 23:26:48 +0900

    article detail 위한 getArticle에 mysqlutil 적용

--- src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
+++ src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
@@ -18,14 +18,14 @@ public class ArticleDao {
 
 	public List<Article> getArticles() {
 		List<Article> articles = new ArrayList<>();
-		
+
 		SecSql sql = new SecSql();
 		sql.append("SELECT *");
 		sql.append("FROM article");
 		sql.append("ORDER BY id DESC");
-		
+
 		List<Map<String, Object>> articleMapList = MysqlUtil.selectRows(sql);
-		
+
 		for (Map<String, Object> articleMap : articleMapList) {
 			articles.add(new Article(articleMap));
 		}
@@ -33,68 +33,16 @@ public class ArticleDao {
 	}
 
 	public Article getArticle(int inputId) {
-		Connection con = null;
 
-		Article article = null;
-
-		try {
-			String dbmsJdbcUrl = "jdbc:mysql://localhost:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
-			String dbmsJdbcLoginId = "sbsst";
-			String dbmsJdbcLoginPw = "sbs123414";
-
-			// MySQL 드라이버 등록
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-			} catch (ClassNotFoundException e1) {
-				e1.printStackTrace();
-			}
-
-			// 연결 생성
-			try {
-				con = DriverManager.getConnection(dbmsJdbcUrl, dbmsJdbcLoginId, dbmsJdbcLoginPw);
-
-			} catch (SQLException e) {
-				e.printStackTrace();
-
-			}
-
-			String sql = "SELECT * FROM article";
-			sql += " WHERE id = ?";
-
-			try {
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				pstmt.setInt(1, inputId);
-				ResultSet rs = pstmt.executeQuery();
-
-				if (rs.next()) {
-
-					int id = rs.getInt("id");
-					String regDate = rs.getString("regDate");
-					String updateDate = rs.getString("updateDate");
-					String title = rs.getString("title");
-					String body = rs.getString("body");
-					int userId = rs.getInt("userId");
-					int boardId = rs.getInt("boardId");
-
-					article = new Article(id, regDate, updateDate, title, body, userId, boardId);
-
-				}
-
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-		} finally {
-			try {
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		SecSql sql = new SecSql();
+		sql.append("SELECT *");
+		sql.append("FROM article");
+		sql.append("WHERE id = ?", inputId);
+		Map<String, Object> articleMap = MysqlUtil.selectRow(sql);
+		if (articleMap.isEmpty()) {
+			return null;
 		}
-
-		return article;
+		return new Article(articleMap);
 	}
 
 	public void remove(int inputId) {
