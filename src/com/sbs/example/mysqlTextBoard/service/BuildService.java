b78d827d6c012aa4715061861896f373b1fa9966
fourleaves8 <fourleaves8@gmail.com>
2020-12-10 12:57:25 +0900

    문제 해결

--- src/com/sbs/example/mysqlTextBoard/service/BuildService.java
+++ src/com/sbs/example/mysqlTextBoard/service/BuildService.java
@@ -7,9 +7,9 @@ import com.sbs.example.mysqlTextBoard.dto.Article;
 import com.sbs.example.mysqlTextBoard.util.Util;
 
 public class BuildService {
-	
+
 	private ArticleService articleService;
-	
+
 	public BuildService() {
 		articleService = Container.articleService;
 	}
@@ -17,44 +17,46 @@ public class BuildService {
 	public void buildsite() {
 		System.out.println("site/article 폴더 생성");
 		Util.mkdirs("site/article");
-		
+
 		List<Article> articles = articleService.showList();
-		
+
 		for (Article article : articles) {
 			StringBuilder sb = new StringBuilder();
-			
+
 			sb.append("<!DOCTYPE html>");
 			sb.append("<html lang=\"ko\">");
-			
+
 			sb.append("<head>");
 			sb.append("<meta charset=\"UTF-8\">");
 			sb.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-			sb.append("<title>게시물 상세페이지 - " + article.title + "<title>");
+			sb.append("<title>게시물 상세페이지 - " + article.title + "</title>");
 			sb.append("</head>");
-			
+
 			sb.append("<body>");
-			
+
 			sb.append("<h1>게시물 상세페이지</h1>");
-			
+
 			sb.append("<div>");
-			
+
 			sb.append("번호 : " + article.id + "<br>");
 			sb.append("생성날짜 : " + article.regDate + "<br>");
 			sb.append("갱신날짜 : " + article.updateDate + "<br>");
 			sb.append("제목 : " + article.title + "<br>");
 			sb.append("내용 : " + article.body + "<br>");
-			
+
 			sb.append("</div>");
-			
+
 			sb.append("</body>");
-			
+
 			sb.append("</html>");
-			
+
 			String fileName = article.id + ".html";
 			String filePath = "site/article/" + fileName;
 			String body = sb.toString();
+
 			Util.fileWriter(filePath, body);
 			System.out.println(filePath + "가 생성되었습니다.");
+			
 		}
 	}
 
